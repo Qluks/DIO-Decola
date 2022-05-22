ce3ed17f9a9070ecf484ecfc2630aa218b5c8e69
Qluks <lucasmmedeirosm@gmail.com>
2022-05-22 13:43:40 -0300

    Primeiro projeto em JS com intregração HTML e CSS

--- Módulo 3/DIO/assets/js/script.js
+++ Módulo 3/DIO/assets/js/script.js
@@ -1,1 +1,29 @@
-console.log("Hello World!");
+var currentNumberWrapper = document.getElementById('currentNumber');
+var currentNumber = 0;
+var buttonAdicionar = document.querySelector(".adicionar");
+var buttonSubtrair = document.querySelector(".subtrair");
+
+currentNumber.addEventListener("click", increment);
+currentNumber.addEventListener("click", decrement);
+
+function increment() {
+    currentNumber = currentNumber + 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+    if (currentNumber >= 10) {
+        buttonAdicionar.disabled = true;
+    } else if (currentNumber < 0){
+        currentNumberWrapper.style.color = "red";
+        buttonAdicionar.disabled = false;
+    } 
+}
+
+function decrement() {
+    currentNumber = currentNumber - 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+    if (currentNumber <= -5) {
+        buttonSubtrair.disabled = true;
+    } else if (currentNumber < 0){
+        currentNumberWrapper.style.color = "red";
+        buttonSubtrair.disabled = false;
+    }
+}
