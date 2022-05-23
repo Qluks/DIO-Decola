ed03b3108f14f9bcc272454be2b9fe43d61483c0
Qluks <lucasmmedeirosm@gmail.com>
2022-05-23 18:54:45 -0300

    Sintaxe JS

---
+++ Módulo 3/Javascript/Sintaxe JS/Aula 2/script.js
@@ -0,0 +1,58 @@
+/*//Boolean
+var vOrF = false;
+console.log(typeof(vOrF));
+
+//Number
+var numeroQualquer = 1;
+console.log(typeof(numeroQualquer));
+
+//String
+var nome = 'Lucas';
+console.log(typeof(nome));*/
+
+/*//Variáveis
+var variavel = 'Lucas'
+variavel = 'Marcos'
+console.log(variavel);
+
+let variavel2 = 'Medeiros';
+variavel2 = 'Lídia';
+console.log(variavel2);
+
+const constante = 'Maria';
+console.log(constante);*/
+
+/*//Escopo
+var escopoGlobal = 'Global';
+console.log(escopoGlobal);
+
+function escopoLocal() {
+    let escopoLocalInterno = 'Local';
+    console.log(escopoLocalInterno);
+}
+escopoLocal();*/
+
+/*//Atribuição
+var atribuicao = 'Lucas';
+
+//Comparação
+var comparacao = '0' == 0;
+console.log(comparacao);
+
+//Comparação Idêntica
+var comparacaoIdentica = '0' === 0;
+console.log(comparacaoIdentica);*/
+
+/*//Potenciação
+var potencia = 2 ** 2;
+console.log(potencia);*/
+
+/*//Operadores Lógicos
+var e = true && false;
+console.log(e);
+
+var ou = true || false;
+console.log(ou);
+
+var nao = !true;
+console.log(nao);*/
---
+++ Módulo 3/Javascript/Sintaxe JS/Aula 3/script.js
@@ -0,0 +1,57 @@
+//Arrays
+//let array = ['String', 1, true];
+//console.log(array);
+
+//let array2 = ['String', 1, true,[5, 10]];
+//console.log(array2[3]);
+
+//forEach
+/*array2.forEach(function (item, index){
+    console.log(item, index);
+});*/
+
+//Push
+/*array2.push('novo item');
+console.log(array2);*/
+
+//Pop
+/*array2.pop();
+console.log(array2);*/
+
+//Shift
+/*array2.shift();
+console.log(array2);*/
+
+//Unshift
+/*array2.unshift('Novo item no inícia');
+console.log(array2);*/
+
+//IndexOF
+//console.log(array2.indexOf(true));
+
+//Splice
+/*array2.splice(0, 2);
+console.log(array2);*/
+
+//Slice
+/*let novoArray = array2.slice(0, 2);
+console.log(novoArray);*/
+
+
+//Objetos
+/*let object = {
+    string: 'String',
+    number: 1,
+    boolean: true,
+    array: [0, 1],
+    objectInterno: {
+        objectInterno: 'Objeto Interno'
+    }
+}
+console.log(object.objectInterno);
+
+var string = object.string;
+console.log(string);
+
+var array = object.array;
+console.log(array);*/
